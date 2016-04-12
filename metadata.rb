8fbc77eae665695a0db0eb2d068e4e13a4e3140f
Matt Ray <matthewhray@gmail.com>
2016-04-12 12:56:37 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name 'mesosphere'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'partnereng@chef.io'
+license 'Apache 2.0'
+description 'Installs/Configures Mesosphere'
+long_description 'Installs/Configures Mesosphere'
+version '0.1.0'
+
+supports 'centos'
+supports 'oracle'
+supports 'redhat'
+supports 'scientific'
+
+depends 'docker', '~> 2.0'
