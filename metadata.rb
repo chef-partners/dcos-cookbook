26d628ded73da4d7856f95068bb72616c0d5247c
Matt Ray <matthewhray@gmail.com>
2016-04-15 15:43:44 -0500

    FC064 and FC065 fixes for adding source_url and issues_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures Mesosphere'
 long_description 'Installs/Configures Mesosphere'
 version '0.1.0'
 
+source_url 'https://github.com/chef-partners/dcos-cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-partners/dcos-cookbook/issues' if respond_to?(:issues_url)
+
 supports 'centos'
 supports 'oracle'
 supports 'redhat'
