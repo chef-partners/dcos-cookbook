c0df0d47857522a304fa0df9b6953b9a8a37cc42
Chris Gianelloni <wolf31o2@gmail.com>
2018-07-23 18:25:33 -0400

    Use latest Docker cookbook
    Switch to the latest version of the docker and selinux cookbooks.
    This is artificially version bumped to get berks to resolve it
    more easily. Since this affects the compatible Chef versions, I am
    going to major version bump for release.
    
    Signed-off-by: Chris Gianelloni <wolf31o2@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'partnereng@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures Mesosphere DC/OS'
 long_description 'Installs/Configures Mesosphere DC/OS'
-version          '1.3.2'
+version          '1.3.3'
 
 source_url 'https://github.com/chef-partners/dcos-cookbook' if
   respond_to?(:source_url)
 issues_url 'https://github.com/chef-partners/dcos-cookbook/issues' if
   respond_to?(:issues_url)
-chef_version '>= 12.1' if
+chef_version '>= 12.10' if
   respond_to?(:chef_version)
 
 %w(
@@ -22,6 +22,5 @@ chef_version '>= 12.1' if
   supports distro
 end
 
-depends 'chef-yum-docker', '~> 3.0'
-depends 'docker', '~> 2.0'
-depends 'selinux', '~> 1.0.4'
+depends 'docker', '~> 4.4'
+depends 'selinux', '~> 2.1'
